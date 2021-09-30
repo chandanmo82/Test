e8307feee28e6b5fd99c95b470c9aaa38acd3005
Chandan <chandanmohanty11111>
2021-09-30 11:31:11 +0530

    First Test

---
+++ src/com/company/TicTacToe.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome To TicTacToe Game");
+        boardPrinting();
+
+    }
+
+    static void boardPrinting() {
+        char[] board = new char[10];
+        int i;
+        for (i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+    }
+}
